3defecf7d67db36afd7b78f47c9910c399f24385
Baris Gumustas <barisgumustas@gmail.com>
2011-11-17 19:39:58 +0200

    some changes and support for video and nfsw data
    video support is not complete atm.

--- app.rb
+++ app.rb
@@ -21,13 +21,19 @@ get '/' do
 end
 
 get '/hot/?:page?' do
-	params[:page] ||= 1
-	parse_list_document("http://9gag.com/hot/#{params[:page]}").to_json
+	url = "http://9gag.com/hot"
+	unless params[:page].nil?
+		url += "/#{params[:page]}"
+	end
+	parse_list_document(url).to_json
 end
 
 get '/trending/?:page?' do
-	params[:page] ||= 1
-	parse_list_document("http://9gag.com/trending/#{params[:page]}").to_json
+	url = "http://9gag.com/trending"
+	unless params[:page].nil?
+		url += "/#{params[:page]}"
+	end
+	parse_list_document(url).to_json
 end
 
 get '/gag/:id' do
@@ -45,13 +51,19 @@ get '/user/:username' do
 end
 
 get '/user/:username/posts/?:page?' do
-	params[:page] ||= 1
-	parse_list_document("http://9gag.com/#{params[:username]}/#{params[:page]}").to_json
+	url = "http://9gag.com/#{params[:username]}"
+	unless params[:page].nil?
+		url += "/#{params[:page]}"
+	end
+	parse_list_document(url).to_json
 end
 
 get '/user/:username/likes/?:page?' do
-	params[:page] ||= 1
-	parse_list_document("http://9gag.com/#{params[:username]}/likes/#{params[:page]}").to_json
+	url = "http://9gag.com/#{params[:username]}/likes"
+	unless params[:page].nil?
+		url += "/#{params[:page]}"
+	end
+	parse_list_document(url).to_json
 end
 
 def fetch_url url
@@ -76,19 +88,32 @@ def parse_list_document url
 	end
 	{
 		:count => items.length,
-		:has_next => (doc.css('#block-content #jump_next:not(.disabled)').length > 0),
+		:has_older => (doc.css('#block-content #jump_next:not(.disabled)').length > 0),
+		:has_newer => (doc.css('#block-content #jump_prev:not(.disabled)').length > 0),
 		:items => items
 	}
 end
 
 def html_to_obj block
-	{
+	item = {
 		:id => block['gagid'],
 		:text => block['data-text'],
 		:url => block['data-url'],
-		:thumbnail => block.css('.content a .img-wrap img').first['src'],
-		:full => block.css('.content a .img-wrap img').first['src'].gsub("_460s.jpg","_700b.jpg"),
 		:poster => block.css('.info>h4>a').first.content,
 		:loved => block.css('.info>p>span.loved').first.content,
 	}
+
+	img = block.css('div.content img').first
+	if img.nil?
+		# video
+		item[:type] = 'video'
+	else
+		# image
+		item[:thumbnail] = img['src']
+		item[:full] = img['src'].gsub("_460s.jpg","_700b.jpg")
+		item[:type] = 'image'
+		item[:is_nsfw] = (img['alt'] == "NSFW")
+	end
+
+	return item
 end
